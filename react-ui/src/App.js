788c42fcc0cac980e56173791466a509c7868649
Mark Oliver <mark.c.oliver@gmail.com>
2020-04-08 13:14:49 -0400

    login

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { login } from './actions'
 
 function App() {
+  console.log('Hello??')
+  login({ username: 'm', password: 'p' })
   return (
     <div className="App">
       <header className="App-header">
